afd83c6684417c18ce4b8163f22a619be2520232
tgxn <domenic@tgxn.net>
2019-06-22 21:37:39 +0800

    update convert to work with magic

--- convert.js
+++ convert.js
@@ -13,10 +13,8 @@
     inputFile = 'LocationHistory.json', // Set the default name for the input file
     outputFile = 'locations.js', // Set the default name for the output file
     locationsVariable = 'locations', // Name of variable that holds the locations
-    mapVariable = 'map', // Name of variable that holds the map focus point and zoom level
     divisor = 10000000, // Divisor to convert the Google geo coordinates to the standard formt
-    zoomFactor = 1.5, // Value by which the calculated zoom level is multiplyed
-    radius = 25; // Default radius for a single location 
+    zoomFactor = 1.5; // Value by which the calculated zoom level is multiplyed
 
     // Read the file asynchronously and trigger callback
     fs.readFile(inputFile, handleFile);
@@ -43,37 +41,35 @@
     function convertData(data) {
 
         var locations = data.locations,
-            len = locations.length,
             convertedData = [],
             mapFocus = {},
-            fileContent = '',
-            latMin = Infinity,
-            latMax = null,
-            longMin = Infinity,
-            longMax = null;
-        
-        for (var i = 0; i < len; i++) {
+            fileContent = '';
+
+        for (var i = 0; i < locations.length; i++) {
+
+            let thisEntry = locations[i];
+
+            // filter > 100
+            if(thisEntry.accuracy > 100) {
+                continue;
+            }
 
             // Normalize the coordinates ...
-            var latitude = locations[i].latitudeE7 / divisor,
-                longitude = locations[i].longitudeE7 / divisor;
+            var latitude = thisEntry.latitudeE7 / divisor,
+                longitude = thisEntry.longitudeE7 / divisor;
 
-            // Get the extrema for latitude and longitude
-            if (latitude < latMin) { latMin = latitude; }
-            if (latitude > latMax) { latMax = latitude; }
-            if (longitude < longMin) { longMin = longitude; }
-            if (longitude > longMax) { longMax = longitude; }
+            let activityType = '';
+            if(typeof thisEntry.activity == "object") {
+                activityType = thisEntry.activity[0].activity[0].type;
+            }
 
-            convertedData.push([latitude, longitude, radius]);
+            convertedData.push([latitude, longitude, thisEntry.timestampMs, thisEntry.accuracy, activityType]);
         }
 
-        mapFocus = calculateMapFocus(latMin, latMax, longMin, longMax);
-
         // Make a string out of all data
-        fileContent += 'var ' + locationsVariable + ' = ' + JSON.stringify(convertedData) + ';\n';
-        fileContent += 'var ' + mapVariable + ' = ' + JSON.stringify(mapFocus) + ';';
+        fileContent += 'var ' + locationsVariable + ' = ' + JSON.stringify(convertedData) + ';';
 
-        saveFile(fileContent, len);
+        saveFile(fileContent, convertedData.length);
     }
 
     function saveFile(data, quantity) {
@@ -90,16 +86,5 @@
             }
         });
     }
-
-    function calculateMapFocus(latMin, latMax, longMin, longMax) {
-
-        return {
-            // The starting point for the map is calculated by the maximum/minimum latitude/longitude/
-            lat : ((latMax - latMin) / 2) + latMin,
-            long: ((longMax - longMin) / 2) + longMin,
-            // The zoom level is calculated using the Pythagorean theorem
-            zoom: Math.round(Math.sqrt(Math.pow(latMax - latMin, 2) + Math.pow(longMax - longMin, 2)) / zoomFactor)
-        };
-    }
-
+    
 }());
